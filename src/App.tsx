afb4df24209d87d198decfb7855fbc11202a7bb5
quynhnn <quynhnn@bravebits.vn>
2024-03-18 00:40:49 +0700

    setup prj

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import HeaderBar from './components/HeaderBar';
+
+const Home = React.lazy(() => import('./views/Home'));
+const ProductsPage = React.lazy(() => import('./views/ProductsPage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <HeaderBar/>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
