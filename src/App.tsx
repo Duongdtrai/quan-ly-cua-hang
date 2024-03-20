aa0a54b1447dd0d75ba0bfe9344759a306841335
sonbh <sonbh@bravebits.vn>
2024-03-20 13:03:45 +0700

    Add product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import HeaderBar from './components/HeaderBar';
+import React, { Suspense } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HeaderBar from "./components/HeaderBar";
+import AddProduct from "./views/product/add";
+import EditProduct from "./views/product/edit";
 
-const Home = React.lazy(() => import('./views/Home'));
-const ProductsPage = React.lazy(() => import('./views/ProductsPage'));
+const Home = React.lazy(() => import("./views/Home"));
+const ProductPage = React.lazy(() => import("./views/product"));
 
 function App() {
   return (
     <Router>
       <div>
-        <HeaderBar/>
+        <HeaderBar />
 
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products" element={<ProductPage />} />
+            <Route path="/product/add" element={<AddProduct />} />
+            <Route path="/product/:id" element={<EditProduct />} />
           </Routes>
         </Suspense>
       </div>
